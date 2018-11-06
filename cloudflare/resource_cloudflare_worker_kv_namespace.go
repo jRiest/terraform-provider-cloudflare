a18570ef9fb5786f2baf1854333579cd3791ba64
Jake Riesterer <jriesterer@cloudflare.com>
2018-11-06 14:52:34 -0800

    Initial workers kv namespace stubs

---
+++ cloudflare/resource_cloudflare_worker_kv_namespace.go
@@ -0,0 +1,29 @@
+package cloudflare
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/cloudflare/cloudflare-go"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/pkg/errors"
+)
+
+func resourceCloudflareWorkerKvNamespace() *schema.Resource {
+}
+
+func resourceCloudflareWorkerKvNamespaceCreate(d *schema.ResourceData, meta interface{}) error {
+}
+
+func resourceCloudflareWorkerKvNamespaceRead(d *schema.ResourceData, meta interface{}) error {
+}
+
+func resourceCloudflareWorkerKvNamespaceUpdate(d *schema.ResourceData, meta interface{}) error {
+}
+
+func resourceCloudflareWorkerKvNamespaceDelete(d *schema.ResourceData, meta interface{}) error {
+}
+
+func resourceCloudflareWorkerKvNamespaceImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+}
